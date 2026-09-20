625ca571bfbec9a0ecfe48f87df1b883c53f423f
IT24101656 <it24101656@my.sliit.lk>
2026-09-20 17:53:50 +0530

    fix

--- agent.py
+++ agent.py
@@ -1,4 +1,8 @@
 # agent.py
+import random
+from collections import deque
+import heapq
+
 class GreedyGridAgent:
     """A simple agent that tries to move around systematically to clear the grid."""
 
